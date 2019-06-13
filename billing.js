c675e6dfd8f458a2da119f601d12e8aeb819b17a
TabbyKat123 <btabbynamwone@gmail.com>
2019-06-13 13:01:58 +0300

    billing command

---
+++ billing.js
@@ -0,0 +1,6 @@
+//this is the percentage that determines the transaction charge
+const transactionCharge = 2.5%;
+
+var amountTendered = 100000;
+console.log ("This is what the billed amount is (less transaction charge)");
+console.log (100000-(0.025*100000));
