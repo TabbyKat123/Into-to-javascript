bf2a4357344b1765f8d9576c35133aa948ab8944
TabbyKat123 <btabbynamwone@gmail.com>
2019-06-14 09:05:55 +0300

    a checker for numbers

---
+++ null_checker.js
@@ -0,0 +1,10 @@
+for (t=1; t<=10; t++)//for the loop to run the count 10 times//
+{
+	let counter=prompt('Enter number:')//the prompt to input numbers//
+	if  (counter>0)
+		{console.log(1);}//brings a number that is greater than 0//
+	else if (counter<0)
+		{console.log(-1);}//brings a number that is less than 0//
+	else
+		{console.log(0);}//outputs 0 if number returned is 0
+}
