668de653bb38f0c9e1094aaf544a49f1d4fd3025
TabbyKat123 <btabbynamwone@gmail.com>
2019-06-17 09:11:30 +0300

    updates

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 var q = Math.floor(Math.random()*100)+1;
+
 var h = q%2;
+
 if (h==0)
 	alert(q+"number is even");
 else
 	alert(q+"number is odd");
 
+
 for (count=1;count<=10;count++){
 	let num= prompt("please input any number:");
-	if (num>q)
-		alert("number is higher, try another one.");
-	else if (num<q)
-		alert("number is lower, try anther one.");
-	else if(num==q)
-		alert("You Win!")
-	else 
-
-		alert("You lose!")
+	if (num == 1){
+		alert("You Win")
+		break;
+	}
+	if (num<h){
+		alert("Number is lower, "+"please try again.");
+	}
+	if (num>h){
+		alert ("Number is higher, "+ "please try again.")
+	}
+	if (count ==10){
+		alert("You Lose!")
+	}
 } 
